f0674abd4b4fc29423416292a3b9be82eac2b9f3
Christopher Hewett <chewett@hotmail.co.uk>
2012-11-15 19:51:05 +0000

    Added ability to return information about single part

--- api.py
+++ api.py
@@ -19,7 +19,17 @@ def _query():
 	matches = query.query(q, inv)
 	
 	return str({"results":[part.code for part in matches]})
-	
-	
+
+@app.route("/part")
+def _part():
+    q = request.query.q
+    q = urllib2.unquote(q)
+    matches = list(query.query("code:" + q, inv))
+    if len(matches) != 1:
+        return "Part code " + q + " apparently has multiple items associated"
+
+    item = matches[0]
+    return str(partToJson(item))
+
 if __name__ == "__main__":
 	run(app=app, port=8080)
