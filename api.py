a814248f9f38673a9fc38ff62df16591ac2a3fd7
Christopher Hewett <chewett@hotmail.co.uk>
2012-11-21 23:52:20 +0000

    Corrected json returned so its valid json and added more info to json

--- api.py
+++ api.py
@@ -1,23 +1,46 @@
-from bottle import Bottle, run, request
+from bottle import Bottle, run, request, static_file
 import urllib2
-import sys, os
+import sys, os, json
 
 sys.path.append(os.path.join(os.path.dirname(__file__), 'tools', 'python', 'inventory'))
 import query, inventory
 
 app = Bottle()
 inv = inventory.Inventory("inventory").root
+PATH = os.path.dirname(os.path.abspath(__file__))
 
 def partToJson(part):
-    return {'code':part.code,
-            'name':part.name}
+
+    if part.__class__.__name__ == 'Item':
+        partInfo = {
+            'type'          : 'Item',
+            'code'          : part.code,
+            'condition'     : part.condition,
+            'description'   : part.description,
+            'labelled'      : str(part.labelled),
+            'name'          : part.name,
+            'path'          : part.path,
+            'value'         : part.value
+        }
+
+    elif part.__class__.__name__ == 'ItemGroup':
+        partInfo = {
+            'type'          : 'ItemGroup',
+            'code'          : part.code,
+            'description'   : part.description,
+            'name'          : part.name,
+            'path'          : part.path
+        }
+
+
+    return json.dumps(partInfo)
 
 @app.route("/query")
 def _query():
 	q = request.query.q
 	q = urllib2.unquote(q)
 	matches = query.query(q, inv)
-	
+
 	return str({"results":[part.code for part in matches]})
 
 @app.route("/part")
@@ -25,11 +48,12 @@ def _part():
     q = request.query.q
     q = urllib2.unquote(q)
     matches = list(query.query("code:" + q, inv))
+
     if len(matches) != 1:
         return "Part code " + q + " apparently has multiple items associated"
 
     item = matches[0]
-    return str(partToJson(item))
+    return partToJson(item)
 
 if __name__ == "__main__":
 	run(app=app, port=8080)
